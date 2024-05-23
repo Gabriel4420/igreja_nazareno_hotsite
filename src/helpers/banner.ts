c0ad7d4483a70e2235ab5b5cb67acf951a8777e9
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-05-23 19:13:04 -0300

    feat: :clown_face: helper for banner images

---
+++ src/helpers/banner.ts
@@ -0,0 +1,10 @@
+
+
+export const file = [
+    { alt: "banner do culto da familia", url: "/images/banner.jpg" },
+    { alt: "banner do culto de jovens", url: "/images/jovens.jpg" },
+    {
+      alt: "banner genérico escrito Slide 3",
+      url: "https://via.placeholder.com/800x400?text=Slide+3",
+    },
+  ];
