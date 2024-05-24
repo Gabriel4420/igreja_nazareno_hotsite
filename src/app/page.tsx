7eb0d31e4dcec8c1908053f82f11f07fcbc9836e
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-05-23 23:41:37 -0300

    refactor: :fire: fixing the wrong code styles classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
+import BlogArea from "@/components/blogArea";
 import Carousel from "@/components/carousel";
 import Header from "@/components/header";
+import Newsletter from "@/components/newsletter";
 import SobreNos from "@/components/sobrenos";
 import { file } from "@/helpers/banner";
 import Image from "next/image";
 
-
 export default function Home() {
-  
-
   return (
     <>
       <Carousel data={file} />
       <main className="flex min-h-screen flex-col items-center justify-between my-10 p-15 md:lg:p-24">
-        <h1 className="text-[46px] md:lg:text-[56px] text-center text-[#E27617] drop-shadow-lg font-bold mb-20 antialiased">
+        <h1 className="text-[32px] py-5 md:lg:text-[56px] text-center text-[#E27617] drop-shadow-lg font-bold mb-20 antialiased">
           Venha cultuar conosco e se aproximar um pouco mais de Deus{" "}
         </h1>
         <Image
@@ -22,10 +21,10 @@ export default function Home() {
           width="500"
           className="mx-auto drop-shadow-lg rounded-sm mb-20"
         />
-        
-        
       </main>
-      <SobreNos/>
+      <SobreNos />
+      <BlogArea />
+      <Newsletter />
     </>
   );
 }
